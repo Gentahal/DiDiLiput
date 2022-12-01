72a125187ca32726be65557761b4b230aaed6283
AbdiRabbani <abdirabbani59@gmail.com>
2022-12-01 09:18:16 +0700

    search network and tabbar network

---
+++ app/src/main/java/com/idn/diliput/network/search/ApiService.kt
@@ -0,0 +1,17 @@
+package com.idn.diliput.network.search
+
+import com.idn.diliput.response.ArticlesItem
+import com.idn.diliput.response.ResultsItem
+import io.reactivex.rxjava3.core.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("/news?apikey=pub_140045eae9852ac5f3fb4035ce1da28cf65a2&")
+    fun getSearchNews(
+        @Query("q") Search : String?
+    ) : Flowable<List<ResultsItem>>
+
+}
